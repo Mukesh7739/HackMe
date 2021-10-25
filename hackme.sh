bcbbe18aa419411c8b29da1e651fc4a7a9efbe93
Mr Kumar <72251189+Mrkumar98@users.noreply.github.com>
2021-10-25 16:19:03 +0530

    Update hackme.sh
    added some simple commands ...

--- hackme.sh
+++ hackme.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Working on project...."
+echo "Starting Pentest... Just Wait"
+#Some Inputs
+read -p "Enter IP or URL :" ip;
+echo "We are Pentesting on '$ip'"
+rm -rf nMap
+rm -rf nikto.log
+mkdir nMap
+nmap -p- -Pn -T4 -oN nMap/initial $ip;
+nikto -h $ip | tee nikto.log;
+enum4linux -a $ip | tee nikto.log
